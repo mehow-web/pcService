c05db647996c6a88a44a672bedabe07962fd7e41
mehow-web <mehow@MacBook-Pro-Micha.local>
2020-09-27 12:55:58 +0200

    Add productplate animations

--- animations.js
+++ animations.js
@@ -1,3 +1,13 @@
 const sidebar = document.querySelector(".sidebar");
 const tl = gsap.timeline({delay: 0.2});
 tl.fromTo(sidebar, 0.8, {x:"-100%"}, {x: "0%"});
+
+const buttons = document.querySelectorAll(".button");
+const plates = document.querySelectorAll(".productplate");
+
+function onFocus(){
+    for(let i; i=0;i++){
+        buttons[i].addEventListener("click", productplate.style.transfrom.tranlate = "scale(1.3)");
+    }
+    
+};

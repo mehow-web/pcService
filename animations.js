f8dfd84beed198296c58d2f794217679492bad20
mehow-web <mk1kwiatkowski@gmail.com>
2020-09-28 20:06:28 +0200

    Add article animations

--- animations.js
+++ animations.js
@@ -1,3 +1,18 @@
 const sidebar = document.querySelector(".sidebar");
 const tl = gsap.timeline({delay: 0.2});
 tl.fromTo(sidebar, 0.8, {x:"-100%"}, {x: "0%"});
+
+const real = document.querySelectorAll(".realization");
+// width: calc(-(100%/3) - 10%);
+let i = 0;
+window.addEventListener("click", function(){
+    while(i < real.length){
+        if(real[i] == real[1]){
+            tl.to(real[1], 0.5, {x: "-150%"});
+        } else {
+            tl.to(real[0], 0.5, {x: "150%"});
+            tl.to(real[2], 0.5, {x: "150%"});
+        }
+        i++;
+    }
+});
